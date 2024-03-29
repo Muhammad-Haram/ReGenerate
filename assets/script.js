ffb2efd12a97cbfbc83e2f92e29fd0fee269982b
Muhammad Haram <haramh643@gmail.com>
2024-03-29 11:35:51 +0000

    dd

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
-console.log("hello");
+var swiper = new Swiper(".mySwiper", {
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    mousewheel: true,
+    keyboard: true,
+});
